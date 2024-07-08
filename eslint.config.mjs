9d85d39d0222990661c23777a14f3b4d7cf2d84b
LuckyLuxem <alexvolkov123098@gmail.com>
2024-07-08 11:19:59 +0300

    feat(base components): added prettier and eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+import pluginJs from '@eslint/js';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReactConfig,
+  eslintPluginPrettier,
+];
