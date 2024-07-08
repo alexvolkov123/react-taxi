2115aeb08c751fc230ea83fc5e4f9b166eddc9f9
LuckyLuxem <alexvolkov123098@gmail.com>
2024-07-08 11:23:12 +0300

    feat(base components): removed webFontLoader and added imports of fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
 import { ThemeProvider } from '@emotion/react'
-import { useEffect } from 'react'
 import { RouterProvider } from 'react-router-dom'
-import WebFont from 'webfontloader'
 
 import { router } from './router/router'
 import { useTheme } from './theme/useTheme'
 
 export const App = () => {
 	const { theme } = useTheme()
-
-	useEffect(() => {
-		WebFont.load({
-			google: {
-				families: ['Roboto', 'Rasa', 'Rubik'],
-			},
-		})
-	}, [])
 
 	return (
 		<ThemeProvider theme={theme}>
