b6b17ee72424daea86d8f73a320320f74698841b
LuckyLuxem <alexvolkov123098@gmail.com>
2024-07-08 12:07:06 +0300

    feat(base components): removed unnecessary memoization

--- src/theme/useTheme.ts
+++ src/theme/useTheme.ts
@@ -1,9 +1,9 @@
 import { Theme, createTheme } from '@mui/material'
-import { useMemo } from 'react'
+
 import { themeSettings } from './theme-settings'
 
 export const useTheme = () => {
-	const theme = useMemo((): Theme => createTheme(themeSettings), [])
+	const theme = (): Theme => createTheme(themeSettings)
 
 	return { theme }
 }
