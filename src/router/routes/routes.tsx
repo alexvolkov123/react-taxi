b17411f9151972bea1267603208ab05bd0379639
LuckyLuxem <alexvolkov123098@gmail.com>
2024-07-09 17:25:53 +0300

    feat(sign up): created Sign Up component and updated routes

--- src/router/routes/routes.tsx
+++ src/router/routes/routes.tsx
@@ -1,15 +1,27 @@
-import { RouteObject } from 'react-router-dom'
+import { Navigate, RouteObject } from 'react-router-dom'
 
-import { SignIn } from '../../pages'
+import { SignIn, SignUp } from '../../pages'
 import { Layout } from '../../shared/components'
 import { RoutePathsEnum } from './routes.types'
 
 export const routes: RouteObject[] = [
   {
     path: RoutePathsEnum.root,
+    element: <Navigate to={RoutePathsEnum.signIn} />,
+  },
+  {
+    path: RoutePathsEnum.signIn,
     element: (
       <Layout>
         <SignIn />
+      </Layout>
+    ),
+  },
+  {
+    path: RoutePathsEnum.signUp,
+    element: (
+      <Layout>
+        <SignUp />
       </Layout>
     ),
   },
