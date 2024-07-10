ffd5ba93632d905db4ec27d342278d2e8bfb5ffb
LuckyLuxem <alexvolkov123098@gmail.com>
2024-07-10 18:39:32 +0300

    feat(sign in): moved provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { ThemeProvider } from '@emotion/react'
-import { Provider } from 'react-redux'
 import { RouterProvider } from 'react-router-dom'
 import { ToastContainer, Zoom } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
 import { useTypedSelector } from './hooks'
 import { router } from './router'
-import { Spinner } from './shared/components'
-import { store } from './store/store'
-import { useTheme } from './theme/useTheme'
+import { Spinner } from './shared'
+import { useTheme } from './theme'
 
 export const App = () => {
   const { theme } = useTheme()
@@ -16,23 +14,21 @@ export const App = () => {
 
   return (
     <ThemeProvider theme={theme}>
-      <Provider store={store}>
-        <RouterProvider router={router} />
+      <RouterProvider router={router} />
 
-        {isLoading && <Spinner />}
+      {isLoading && <Spinner />}
 
-        <ToastContainer
-          position={'bottom-right'}
-          autoClose={2}
-          limit={2}
-          hideProgressBar={false}
-          closeOnClick={true}
-          pauseOnHover={true}
-          draggable={true}
-          theme={'light'}
-          transition={Zoom}
-        />
-      </Provider>
+      <ToastContainer
+        position={'bottom-right'}
+        autoClose={2}
+        limit={2}
+        hideProgressBar={false}
+        closeOnClick={true}
+        pauseOnHover={true}
+        draggable={true}
+        theme={'light'}
+        transition={Zoom}
+      />
     </ThemeProvider>
   )
 }
