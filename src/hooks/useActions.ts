ce99345a8b192f8779666304190bc12214dfb905
LuckyLuxem <alexvolkov123098@gmail.com>
2024-07-10 18:09:23 +0300

    feat(sign in): added hooks

--- src/hooks/useActions.ts
+++ src/hooks/useActions.ts
@@ -2,10 +2,14 @@ import { useMemo } from 'react'
 import { useDispatch } from 'react-redux'
 import { bindActionCreators } from 'redux'
 
-import { rootActions } from '../store/actions/root-actions'
+import { loadingActions } from '../store'
 
 export const useActions = () => {
-	const dispatch = useDispatch()
+  const dispatch = useDispatch()
 
-	return useMemo(() => bindActionCreators(rootActions, dispatch), [dispatch])
+  const rootActions = {
+    ...loadingActions,
+  }
+
+  return useMemo(() => bindActionCreators(rootActions, dispatch), [dispatch])
 }
