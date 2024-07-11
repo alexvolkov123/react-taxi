8723c9501547a538fdd66af09483126ad336a28a
LuckyLuxem <alexvolkov123098@gmail.com>
2024-07-11 18:33:40 +0300

    feat(sign in): added auth form title

--- src/pages/sign-in/sign-in.tsx
+++ src/pages/sign-in/sign-in.tsx
@@ -1,5 +1,12 @@
+import { AuthFormTitle } from '../../shared/components'
 import { SignInForm } from './sign-in-form'
+import './sign-in.scss'
 
 export const SignIn = () => {
-  return <SignInForm />
+  return (
+    <div className='sign-in'>
+      <AuthFormTitle title='Sign In' />
+      <SignInForm />
+    </div>
+  )
 }
