a6e4a9831b87c918d79168697b5334a6fa00911c
LuckyLuxem <alexvolkov123098@gmail.com>
2024-07-11 16:57:21 +0300

    feat(sign in):  changed getting of languages

--- src/shared/components/layout/header/header-links/header-links.tsx
+++ src/shared/components/layout/header/header-links/header-links.tsx
@@ -1,24 +1,18 @@
 import { Link } from '@mui/material'
-import { useCallback, useMemo, useState } from 'react'
+import { useCallback } from 'react'
 import { useNavigate } from 'react-router-dom'
 
+import { useLanguages, useTypedSelector } from '../../../../../hooks'
 import { RoutePathsEnum } from '../../../../../router'
 import { menuSettings } from '../../../../../theme'
 import { BaseSelect } from '../../../selects'
 import './header-links.scss'
 
-const initLanguages = ['English', 'Russian', 'German']
-
 export const HeaderLinks = () => {
   const navigate = useNavigate()
-  const [language, setLanguage] = useState('Russian')
+  const { language, languages, setLanguage } = useLanguages()
 
-  const isOpen = false
-
-  const languages = useMemo(
-    () => [language, ...initLanguages.filter(item => item !== language)],
-    [language]
-  )
+  const { userTokens } = useTypedSelector(state => state.auth)
 
   const handleChangeLanguage = (value: string) => setLanguage(value)
 
@@ -37,7 +31,7 @@ export const HeaderLinks = () => {
           isSelectedItemHidden={true}
           onChange={handleChangeLanguage}
         />
-        {isOpen && <Link onClick={logout}>Log Out</Link>}
+        {userTokens && <Link onClick={logout}>Log Out</Link>}
       </div>
     </div>
   )
